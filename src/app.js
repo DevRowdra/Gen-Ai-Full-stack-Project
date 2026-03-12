39f9a0db47ac4b75c2c1bc6954788fc0532839a3
DevRowdra <rowdradev23@gmail.com>
2026-03-12 10:28:49 +0600

    set up project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+module.exports = app;
